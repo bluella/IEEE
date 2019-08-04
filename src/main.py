e60f434f3b1833ac9b97688f428142b4f1055ddd
Igor Grigorev <barsic1080@gmail.com>
2019-08-04 20:37:47 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,84 @@
+folder_path = '../input/'
+train_identity = pd.read_csv(f'{folder_path}train_identity.csv')
+train_transaction = pd.read_csv(f'{folder_path}train_transaction.csv')
+test_identity = pd.read_csv(f'{folder_path}test_identity.csv')
+test_transaction = pd.read_csv(f'{folder_path}test_transaction.csv')
+sub = pd.read_csv(f'{folder_path}sample_submission.csv')
+# let's combine the data and work with the whole dataset
+train = pd.merge(train_transaction, train_identity, on='TransactionID', how='left')
+test = pd.merge(test_transaction, test_identity, on='TransactionID', how='left')
+
+
+print(f'Train dataset has {train.shape[0]} rows and {train.shape[1]} columns.')
+print(f'Test dataset has {test.shape[0]} rows and {test.shape[1]} columns.')
+
+print(train.head())
+print(test.head())
+
+del train_identity, train_transaction, test_identity, test_transaction
+
+print(f'There are {train.isnull().any().sum()} columns in train dataset with missing values.')
+
+
+
+
+# check specific feature
+
+train['id_11'].value_counts(dropna=False, normalize=True).head()
+plt.hist(train['id_07'])
+plt.title('Distribution of id_07 variable')
+
+
+# compare train and test distributions 
+
+plt.hist(train['TransactionDT'], label='train')
+plt.hist(test['TransactionDT'], label='test')
+plt.legend()
+plt.title('Distribution of transactiond dates')
+
+# drop columns 
+
+one_value_cols = [col for col in train.columns if train[col].nunique() <= 1]
+one_value_cols_test = [col for col in test.columns if test[col].nunique() <= 1]
+many_null_cols = [col for col in train.columns if train[col].isnull().sum() / train.shape[0] > 0.9]
+many_null_cols_test = [col for col in test.columns if test[col].isnull().sum() / test.shape[0] > 0.9]
+big_top_value_cols = [col for col in train.columns if train[col].value_counts(dropna=False, normalize=True).values[0] > 0.9]
+big_top_value_cols_test = [col for col in test.columns if test[col].value_counts(dropna=False, normalize=True).values[0] > 0.9]
+cols_to_drop = list(set(many_null_cols + many_null_cols_test + big_top_value_cols + big_top_value_cols_test + one_value_cols+ one_value_cols_test))
+cols_to_drop.remove('isFraud')
+len(cols_to_drop)
+train = train.drop(cols_to_drop, axis=1)
+test = test.drop(cols_to_drop, axis=1)
+
+# categorical columns into numbers
+
+for col in cat_cols:
+    if col in train.columns:
+        le = LabelEncoder()
+        le.fit(list(train[col].astype(str).values) + list(test[col].astype(str).values))
+        train[col] = le.transform(list(train[col].astype(str).values))
+        test[col] = le.transform(list(test[col].astype(str).values)) 
+
+
+# train
+n_fold = 5
+folds = TimeSeriesSplit(n_splits=n_fold)
+folds = KFold(n_splits=5)
+params = {'num_leaves': 256,
+          'min_child_samples': 79,
+          'objective': 'binary',
+          'max_depth': 13,
+          'learning_rate': 0.03,
+          "boosting_type": "gbdt",
+          "subsample_freq": 3,
+          "subsample": 0.9,
+          "bagging_seed": 11,
+          "metric": 'auc',
+          "verbosity": -1,
+          'reg_alpha': 0.3,
+          'reg_lambda': 0.3,
+          'colsample_bytree': 0.9,
+          #'categorical_feature': cat_cols
+         }
+result_dict_lgb = train_model_classification(X=X, X_test=X_test, y=y, params=params, folds=folds, model_type='lgb', eval_metric='auc', plot_feature_importance=True,
+                                                      verbose=500, early_stopping_rounds=200, n_estimators=5000, averaging='usual', n_jobs=-1)
